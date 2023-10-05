0c708fe9f2b2e1f1d8503aaddb14c5ac0c0baae9
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2023-10-05 14:36:04 -0400

    clean code

--- App.tsx
+++ App.tsx
@@ -7,41 +7,12 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import CounterScreen from './src/features/counter/screens/CounterScreen';
 import ImageListScreen from './src/features/image-list/screens/ImageListScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store";
 
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
